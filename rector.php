cbf0451211fef3b6f8ecee05dc02fd3279cfc178
snipershady <snipershady@gmail.com>
2023-04-05 14:50:10 +0100

    Rector removed unused class

--- rector.php
+++ rector.php
@@ -8,12 +8,9 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Core\Configuration\Option;
-use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SensiolabsSetList;
-use Rector\Symfony\Set\SymfonySetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
