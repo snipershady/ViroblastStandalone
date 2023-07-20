3b3dcc99ae9dfa62fac43542120adec0e3c17cc9
snipershady <snipershady@gmail.com>
2023-07-20 16:17:42 +0100

    Refactoring logout

--- logout.php
+++ logout.php
@@ -1,9 +1,7 @@
 <?php
 
-//require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/bootstrap.php';
 
-use App\Component\ConfigurationHandler;
 use App\Controller\LoginController;
 
 $logincontroller = new LoginController();
